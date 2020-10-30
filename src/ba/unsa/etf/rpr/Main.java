19589eebcd088c4ff2b1330a0e9ff0cebe21c447
Dzeneta Ahmic <dahmic@etf.unsa.ba>
2020-10-30 18:23:45 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+    public static  int sumaCifara(int a){
+        int suma  = 0;
+        int pom;
+        while (a != 0){
+            pom = a%10;
+            suma += pom;
+            a = a/10;
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Unesite broj n: ");
+        Scanner in =  new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 2; i < n; i++){
+            if(i % sumaCifara(i) == 0){
+                System.out.println(i);
+            }
+        }
+	// write your code here
+    }
+}
